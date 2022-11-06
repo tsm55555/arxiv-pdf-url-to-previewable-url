152f3bcfe5e50c485c70bb818f465e6f47cea303
Anthony Lin <tsm62803@gmail.com>
2022-11-06 20:58:59 +0800

    add __name__

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-arxix_pdf_url = input("input your arxiv pdf url: ")
+def pdf_to_previewable():
+    arxix_pdf_url = input("input your arxiv pdf url: ")
+    previewable_url = arxix_pdf_url.replace(".pdf", "")
+    previewable_url = previewable_url.replace("pdf", "abs")
 
-previewable_url = arxix_pdf_url.replace(".pdf", "")
-previewable_url = previewable_url.replace("pdf", "abs")
+    print("previewable url: ",previewable_url)
 
-print("previewable url: ",previewable_url)
+if __name__ == "__main__":
+    pdf_to_previewable()
